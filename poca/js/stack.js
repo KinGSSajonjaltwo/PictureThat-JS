3640f3cb8b2f026f0af466684c87b900b9452daa
minsop <minsoplee2015@gmail.com>
2022-12-31 01:52:08 +0900

    stack 에서 카드 뒤로가는 효과 추가
    핸드폰에서는 간헐적으로 버벅거림
    이거시 바로 illusion?!?!

--- poca/js/stack.js
+++ poca/js/stack.js
@@ -1,6 +1,5 @@
 let stack = document.querySelector(".stack");
 
-
 // stack of card 만들고 순서 뒤집은 것으로 stack에 초기화
 [...stack.children].reverse().forEach(i => stack.append(i));
 
@@ -10,10 +9,12 @@ function swap(e){
   //가장 앞에 있는 card : card:last-child  
   let card = document.querySelector(".card:last-child");
   if(e.target !== card) return;
+  card.style.animation = "swap 700ms forwards";
 
   setTimeout(() => {
+    card.style.animation = "";
     stack.prepend(card);
-  }, 100)
+  }, 700)
 };
 
 
