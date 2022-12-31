5550e7a91078bc9b66809dff7416dca4a13a0c2c
minsop <minsoplee2015@gmail.com>
2022-12-31 12:52:24 +0900

    next prev 버튼 추가
    next : 앞에 카드 뒤로
    prev : 뒤에 카드 앞으로
    
    아바타?ㅋ ㅋ
    이게 바로 real OG "third - Dimension"

--- poca/js/stack.js
+++ poca/js/stack.js
@@ -1,20 +1,20 @@
 let stack = document.querySelector(".stack");
-
 // stack of card 만들고 순서 뒤집은 것으로 stack에 초기화
 [...stack.children].reverse().forEach(i => stack.append(i));
 
 stack.addEventListener("click", swap);
 
+
 function swap(e){
   //가장 앞에 있는 card : card:last-child  
   let card = document.querySelector(".card:last-child");
   if(e.target !== card) return;
-  card.style.animation = "swap 700ms forwards";
+  card.style.animation = "swapNext 400ms forwards";
 
   setTimeout(() => {
     card.style.animation = "";
     stack.prepend(card);
-  }, 700)
+  }, 400)
 };
 
 
